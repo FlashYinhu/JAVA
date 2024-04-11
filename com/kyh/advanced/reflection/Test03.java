31c21e6206544259b4324ed0bd3cd389547850e2
Yinhu <kangyinhunnu@gmail.com>
2024-04-11 10:48:23 +0800

    4/11

--- com/kyh/advanced/reflection/Test03.java
+++ com/kyh/advanced/reflection/Test03.java
@@ -5,11 +5,19 @@ public class Test03 {
         Person p1 = new Student();
         System.out.println(p1.name);
 
-        Class c1 = p1.getClass();
-        Class c2 = Class.forName("com.kyh.advanced.reflection.Student");
+        Class c1 = p1.getClass(); // 通过对象获得
+        Class c2 = Class.forName("com.kyh.advanced.reflection.Student"); // 通过forname获得
+        Class c3 = Student.class; // 通过类名.class获得
+        Class c4 = Integer.TYPE;// 内置包装类的Type属性
+        Class c5 = c1.getSuperclass(); // 获得学生类的父类
 
         System.out.println(c1.hashCode());
         System.out.println(c2.hashCode());
+        System.out.println(c3.hashCode());
+        System.out.println(c4.hashCode());
+        System.out.println(c5);
+
+
     }
 }
 
